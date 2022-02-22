2af82d4f0de610220abfebaedd8c08786da8a473
leochoo <leochoodev@gmail.com>
2022-02-22 10:39:15 +0900

    sample component separated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import {
   AppShell,
   Burger,
@@ -10,10 +8,9 @@ import {
   Text,
   useMantineTheme,
 } from "@mantine/core";
+import Sample from "./Sample";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const [opened, setOpened] = useState(false);
   const theme = useMantineTheme();
 
@@ -60,42 +57,7 @@ function App() {
       }
     >
       <Text>Resize app to see responsive navbar in action</Text>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello Vite + React!</p>
-          <p>
-            <button
-              type="button"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {" | "}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </header>
-      </div>
+      <Sample />
     </AppShell>
   );
 }
