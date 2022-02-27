c1340773cb4545c0932123c018e2e879c07760c3
leochoo <leochoodev@gmail.com>
2022-02-28 01:08:57 +0900

    display card image

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CardSample from "../components/CardSample";
 import WrapperPage from "./WrapperPage";
 
 const CalendarPage = () => {
   return (
     <WrapperPage>
+      <CardSample />
       <div>CalendarPage</div>
     </WrapperPage>
   );
