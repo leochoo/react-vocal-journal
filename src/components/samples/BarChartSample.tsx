441884de00229bbcebd96f5209e635f3804361bd
leochoo <leochoodev@gmail.com>
2022-03-18 23:44:43 +0900

    filled empty

---
+++ src/components/samples/BarChartSample.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BarChartSample = () => {
+  return <div>BarChartSample</div>;
+};
+
+export default BarChartSample;
---
+++ src/components/samples/Heatmap.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heatmap = () => {
+  return <div>Heatmap</div>;
+};
+
+export default Heatmap;
