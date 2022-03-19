6111ac1de4ea53d6df507b949325982dd35f8f2e
leochoo <leochoodev@gmail.com>
2022-03-19 13:10:41 +0900

    added jitter chart

--- src/components/samples/LineChartSample.tsx
+++ src/components/samples/LineChartSample.tsx
@@ -32,7 +32,7 @@ export const options = {
     },
     title: {
       display: true,
-      text: "Chart.js Line Chart",
+      text: "Jitter Linechart",
     },
   },
 };
@@ -43,13 +43,13 @@ export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
+      label: "1-YR AGO",
       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
-      label: "Dataset 2",
+      label: "Now",
       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,16 +1,94 @@
 import React from "react";
-import { Container, Text, Grid } from "@mantine/core";
+import { Container, Text, Grid, createStyles } from "@mantine/core";
 import CardSample from "../components/samples/CardSample";
 import { RadarChartSample } from "../components/samples/RadarChartSample";
 import { LineChartSample } from "../components/samples/LineChartSample";
+import { StatsRingCard } from "../components/samples/StatsRingCard";
+import { TableReviews } from "../components/samples/TableReviews";
+
+const statsRingCardData = {
+  title: "Vocal Practice",
+  completed: 81,
+  total: 100,
+  stats: [
+    {
+      value: 19,
+      label: "Remaining",
+    },
+    {
+      value: 2,
+      label: "In progress",
+    },
+  ],
+};
+
+const data = [
+  {
+    title: "Foundation",
+    author: "Isaac Asimov",
+    year: 1951,
+    reviews: {
+      positive: 2223,
+      negative: 259,
+    },
+  },
+  {
+    title: "Frankenstein",
+    author: "Mary Shelley",
+    year: 1818,
+    reviews: {
+      positive: 5677,
+      negative: 1265,
+    },
+  },
+  {
+    title: "Solaris",
+    author: "Stanislaw Lem",
+    year: 1961,
+    reviews: {
+      positive: 3487,
+      negative: 1845,
+    },
+  },
+  {
+    title: "Dune",
+    author: "Frank Herbert",
+    year: 1965,
+    reviews: {
+      positive: 8576,
+      negative: 663,
+    },
+  },
+  {
+    title: "The Left Hand of Darkness",
+    author: "Ursula K. Le Guin",
+    year: 1969,
+    reviews: {
+      positive: 6631,
+      negative: 993,
+    },
+  },
+  {
+    title: "A Scanner Darkly",
+    author: "Philip K Dick",
+    year: 1977,
+    reviews: {
+      positive: 8124,
+      negative: 1847,
+    },
+  },
+];
+
 const DashboardPage = () => {
   return (
     <Container size="xl" px="xs">
       <Text size="xl">Dashboard</Text>
 
-      <Grid grow>
-        <Grid.Col sm={12} lg={6}>
-          <CardSample />
+      <TableReviews data={data} />
+
+      <Grid grow style={{ border: "red solid 1px" }}>
+        <Grid.Col style={{ border: "orange solid 1px" }} sm={12} lg={6}>
+          <StatsRingCard {...statsRingCardData} />
         </Grid.Col>
         <Grid.Col
           sm={12}
@@ -20,11 +98,12 @@ const DashboardPage = () => {
             margin: "auto",
             width: "35vw",
             height: "60vh",
+            border: "orange solid 1px",
           }}
         >
-          <RadarChartSample />
+          <LineChartSample />
         </Grid.Col>
-        <Grid.Col sm={12} lg={3}>
+        <Grid.Col style={{ border: "orange solid 1px" }} sm={12} lg={3}>
           <CardSample />
         </Grid.Col>
         <Grid.Col
