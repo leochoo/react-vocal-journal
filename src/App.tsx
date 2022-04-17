816cc6e8fec2d7f94f4d5b9d957cc150a7315c9f
leochoo <leochoodev@gmail.com>
2022-04-17 21:56:18 +0900

    private routes separated but not working

--- src/App.tsx
+++ src/App.tsx
@@ -31,25 +31,10 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 import { selectIsLoggedIn } from "./redux/auth/auth.slice";
 import { useAppSelector } from "./redux/hooks";
+import { ProtectedRoute } from "./components/ProtectedRoutes";
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
-
-  function ProtectedRoute({ children }: { children: JSX.Element }) {
-    let location = useLocation();
-    const isLoggedIn = useAppSelector(selectIsLoggedIn);
-    console.log("isLoggedIn", isLoggedIn);
-
-    if (!isLoggedIn) {
-      // Redirect them to the /login page, but save the current location they were
-      // trying to go to when they were redirected. This allows us to send them
-      // along to that page after they login, which is a nicer user experience
-      // than dropping them off on the home page.
-      return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-
-    return <WrapperPage>{children}</WrapperPage>;
-  }
 
   return (
     // <WrapperPage>
